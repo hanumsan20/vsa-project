14364071bc84afd82a368a044eb5f6729083debc
hanumsan20 <hanumsan20@7hills.org>
2017-06-19 15:16:31 -0500

    Add files via upload

--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Santosh
+# Date: 6/19/2017
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
@@ -10,13 +10,20 @@
 name = raw_input("Enter your name: ")
 age = int(raw_input("Enter your age: "))
 birthday = raw_input("Has your birthday happened this year? Enter Y or N: ")
-
 if birthday == "Y":
-    # Calculates the year that the user will be 100
-    year_100 = str((100 - age) + 2017)
-
+    year = 2017
+    year_100 = (2017 - age) + 100
+    num = 2017
+    while num < (year_100 + 1):
+        print str(num)
+        num = num + 1
 else:
-    # Calculates the year that the user will be 100
-    year_100 = str((100 - age) + 2016)
+    year = 2016
+    year_100 = (2017 - age) + 99
+    num = 2016
+    while num < (year_100 + 1):
+        print str(num)
+        num = num + 1
+# Calculates the year that the user will be 100
+# TO DO: write for or while loop that adds one year to year each time and stops at the year that the user will be 100
 
-print name, " will turn 100 in the year ", year_100, "."
