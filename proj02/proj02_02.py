3a6f677ea3e288444ae94689fda725e48f74b41d
hanumsan20 <hanumsan20@7hills.org>
2017-06-19 15:32:33 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -9,5 +9,14 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
-
-
+count_of_numbers = raw_input("How many Fibonacci numbers do you want to generate? ")
+num = 0
+count1 = 0
+count2 = 1
+count3 = count1 + count2
+while num < int(count_of_numbers):
+    print count1 + count2
+    count3 = count1 + count2
+    count2 = count1
+    count1 = count3
+    num = num + 1
